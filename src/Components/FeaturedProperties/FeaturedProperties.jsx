a9c90b1d42b8689e5d1d8f0b20a2878138e3c028
Asutosh Acharya <asutosh246@gmail.com>
2022-11-23 11:39:13 +0530

    featuredProperties added

---
+++ src/Components/FeaturedProperties/FeaturedProperties.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./featuredProperties.css";
+const FeaturedProperties = () => {
+  return (
+    <div className="fp">
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/123801934.webp?k=27073a18101dd5a4eefc76251f7d476be72e19ed03e98819f2d94667dd60f31a&o=&s=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">MayFair</span>
+        <span className="fpCity">Bhubaneswar</span>
+        <span className="fpPrice">Starting from Rs.12000</span>
+        <div className="fpRating">
+          <button>8.9</button>
+        </div>
+        <span>Excellent</span>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/123801934.webp?k=27073a18101dd5a4eefc76251f7d476be72e19ed03e98819f2d94667dd60f31a&o=&s=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">MayFair</span>
+        <span className="fpCity">Bhubaneswar</span>
+        <span className="fpPrice">Starting from Rs.12000</span>
+        <div className="fpRating">
+          <button>8.9</button>
+        </div>
+        <span>Excellent</span>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/123801934.webp?k=27073a18101dd5a4eefc76251f7d476be72e19ed03e98819f2d94667dd60f31a&o=&s=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">MayFair</span>
+        <span className="fpCity">Bhubaneswar</span>
+        <span className="fpPrice">Starting from Rs.12000</span>
+        <div className="fpRating">
+          <button>8.9</button>
+        </div>
+        <span>Excellent</span>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/123801934.webp?k=27073a18101dd5a4eefc76251f7d476be72e19ed03e98819f2d94667dd60f31a&o=&s=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">MayFair</span>
+        <span className="fpCity">Bhubaneswar</span>
+        <span className="fpPrice">Starting from Rs.12000</span>
+        <div className="fpRating">
+          <button>8.9</button>
+        </div>
+        <span>Excellent</span>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProperties;
--- src/Pages/home/HomePage.jsx
+++ src/Pages/home/HomePage.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import './HomePage.css'
-import Navbar from '../../Components/Navbar/Navbar'
-import Header from '../../Components/Header/Header'
-import Featured from './../../Components/Featured/Featured';
+import React from "react";
+import "./HomePage.css";
+
+import Navbar from "../../Components/Navbar/Navbar";
+import Header from "../../Components/Header/Header";
+import Featured from "./../../Components/Featured/Featured";
+import PropertyList from "../../Components/propertyList/PropertyList";
+import FeaturedProperties from "../../Components/FeaturedProperties/FeaturedProperties";
+
 const HomePage = () => {
   return (
-  <>
-    <Navbar/>
-    <Header/>
-    <div className="homeContainer">
-      <Featured />
-    </div>
-  </>
-  )
-}
+    <>
+      <Navbar />
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+      </div>
+    </>
+  );
+};
 
 export default HomePage;
